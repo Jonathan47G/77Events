2d893d4628cda579d072ac82fa6280261fde61c9
Jonathan47G <guestin.jonathan@gmail.com>
2023-11-15 20:45:59 +0100

    ajout test unitaire / fonctionnel

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,44 +1,65 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, act, waitFor} from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
+	it("a list of fields card is displayed", async () => {
+		render(<Home />);
+		await screen.findByText("Email");
+		await screen.findByText("Nom");
+		await screen.findByText("Prénom");
+		await screen.findByText("Personel / Entreprise");
+	});
 
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-        await screen.findByText("Envoyer"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
-    });
-  });
-
+	describe("and a click is triggered on the submit button", () => {
+		it("the success message is displayed", async () => {
+			render(<Home />);
+			fireEvent(
+				await screen.findByText("Envoyer"),
+				new MouseEvent("click", {
+					cancelable: true,
+					bubbles: true,
+				})
+			);
+			await screen.findByText("En cours");
+			await screen.findByText("Message envoyé !");
+		});
+	});
 });
 
-
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
-  it("a footer is displayed", () => {
-    // to implement
-  })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
-  })
+	it("a list of events is displayed", async () => {
+		// to implement
+		act(() => {
+			render(<Home/>);			
+		  });
+		  await waitFor(() => {
+		 screen.findByText("Conférence #ProductCON", "User&Product MixUsers");
+		  })
+		  
+	});
+	it("a list a people is displayed", async () => {
+		// to implement 
+		render(<Home />);
+		await screen.findByText("Samira", "CEO");
+		await screen.findByText("Alice", "CXO");
+		await screen.findByText("Jean-baptiste", "Directeur Marketing");
+	});
+	it("a footer is displayed", () => {
+		// to implement
+		render(<Home />);
+		 screen.findByText("45 avenue de la République, 75000 Paris");
+		 screen.findByText("01 23 45 67 89");
+		 screen.findByText("contact@77events.com");
+	});
+	it("an event card, with the last event, is displayed", async () => {
+		// to implement
+		act(() => {
+			render(<Home/>);			
+		  });
+		  await waitFor(() => {
+		 screen.findByText("boom");
+		 screen.findByText("Conférence #ProductCON");
+		  })
+		  
+	});
 });
