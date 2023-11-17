935265ef89e21ba10ce20d71281341dce5bb1e1a
Jonathan47G <guestin.jonathan@gmail.com>
2023-11-17 20:51:08 +0100

    mise à jour du test avec given/when/then

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -3,9 +3,14 @@ import {getMonth} from './index'
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            // Given (Données initials)
             const date = new Date('2022-01-01');
-            expect(getMonth(date)).toBe('janvier');
+
+            // When (Execution de la méthode qu'on teste)
+            const mois = getMonth(date);
+
+            // Then (Vérification si résultat OK)
+            expect(mois).toBe('janvier');
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             // to implement
